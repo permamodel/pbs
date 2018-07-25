f0bf27a5b0099d34978be75893a1879d1c9dc032
Mark Piper <mark.piper@colorado.edu>
2018-07-25 12:06:27 -0600

    Set up paths for numpydoc, pbs-server, and pbs=executor

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -11,10 +11,19 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('../../pbs-executor'))
+sys.path.insert(0, os.path.abspath('../../pbs-server'))
+sys.path.insert(0, os.path.abspath('../numpydoc'))
+sys.path.insert(0, os.path.abspath('.'))
+
+print 'Paths:'
+for item in sys.path:
+    print '- {}'.format(item)
 
 
 # -- Project information -----------------------------------------------------
@@ -43,6 +52,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
